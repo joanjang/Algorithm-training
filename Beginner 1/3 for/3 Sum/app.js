c806f992818fd6870052b46663253fab208cbfc5
je0489 <je0489@naver.com>
2021-09-03 23:38:28 +0900

    Add a 7th challenge

---
+++ Beginner 1/3 for/3 Sum/app.js
@@ -0,0 +1,13 @@
+/**
+ * boj.kr #8393
+ * input/output: 3 / 6
+ */
+
+// input = 6
+// output = 1+2+3+..+n
+
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim();
+let sum = 0;
+for (let i = +input; i > 0; i--)
+    sum += i;
+console.log(sum);
