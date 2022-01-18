81f1a3cde2ae58444031337ce02b30a0ab7b0652
je0489 <je0489@naver.com>
2022-01-18 15:38:22 +0900

    Add 66th chanllenge

---
+++ Beginner 2/codility/4 Counting Elements/3 MaxCounters/app.js
@@ -0,0 +1,38 @@
+/**
+ * https://app.codility.com/programmers/lessons/4-counting_elements/max_counters/
+ * input(N/A): 5 / [3, 4, 4, 6, 1, 4, 4]
+ * output: [3, 2, 2, 4, 2]
+ */
+
+//  N and M are integers within the range [1..100,000];
+//  each element of array A is an integer within the range [1..N + 1].
+
+const input = require('fs')
+    .readFileSync(process.platform === 'linux' ? '/dev/stdin' : './input.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+// 시간복잡도: O(N + M)
+const solution = (N, A) => {
+    const result = new Array(N).fill(0);
+    let max = 0,
+        lastMax = 0;
+
+    for (let i = 0; i < A.length; i++) {
+        const val = A[i];
+        if (val <= N) {
+            if (result[val - 1] < lastMax) result[val - 1] = lastMax;
+            if (max < ++result[val - 1]) max = result[val - 1];
+        } else lastMax = max;
+    }
+
+    return result.map((val) => (val < lastMax ? lastMax : val));
+};
+
+console.log(
+    input.map((input) => {
+        const [N, A] = input.split(' ');
+        return solution(+N, A.split(',').map(Number));
+    })
+);
